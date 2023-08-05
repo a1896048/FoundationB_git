00f6975c60fc085ab597c8ded45b151ade182691
lingzen <zengl5335@gmail.com>
2023-08-05 18:46:37 +0930

    ff

--- question08.sh
+++
@@ -1,5 +0,0 @@
-mkdir dir2
-mv *.txt dir2/
-git init
-git add dir2/*.txt
-git commit -m "question08"
--- question09.sh
+++
@@ -1,5 +0,0 @@
-git branch1
-git checkout brain1
-touch file3
-git add file3
-git commit -m "question09"
